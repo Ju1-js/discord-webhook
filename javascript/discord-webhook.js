aa7a53c260fb4172107a35836b1978568a65a1af
Ju1-js <40339350+Ju1-js@users.noreply.github.com>
2023-04-05 18:32:24 -0700

    First alpha release

--- javascript/discord-webhook.js
+++ javascript/discord-webhook.js
@@ -1,3 +1,6 @@
 function image_and_url(gallery) {
-  return [extract_image_from_gallery(gallery), window.location.origin];
+  fetch("http://127.0.0.1:7860/user/")
+    .then((response) => response.json())
+    .then((data) => (user = data));
+  return [extract_image_from_gallery(gallery), user];
 }
